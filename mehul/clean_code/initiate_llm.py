87dafc988e66db73f25d4cf3fcb4826ef99823e1
mehulmathur <mathurmehul3@gmail.com>
2025-04-23 17:32:52 -0500

    added supervisore code

---
+++ mehul/clean_code/initiate_llm.py
@@ -0,0 +1,16 @@
+from dotenv import load_dotenv
+from langchain_community.chat_models.azureml_endpoint import AzureMLChatOnlineEndpoint, AzureMLEndpointApiType, CustomOpenAIChatContentFormatter
+load_dotenv(override=True)
+from langchain_core.messages import HumanMessage
+from langchain_openai import ChatOpenAI
+from langchain_core.messages import BaseMessage
+
+llama_llm =  AzureMLChatOnlineEndpoint(
+    endpoint_url="https://Meta-Llama-3-1-70B-Instruct-zhlk.westus.models.ai.azure.com/chat/completions",
+    endpoint_api_type=AzureMLEndpointApiType.serverless,
+    endpoint_api_key="oOGxXLf70uZz1XgcOL1PKzgNBzP4GshV",
+    content_formatter=CustomOpenAIChatContentFormatter(),
+    model_kwargs={"temperature": 0.3, "max_tokens": 2000}
+)
+
+gpt_llm = ChatOpenAI(model="gpt-4o")
